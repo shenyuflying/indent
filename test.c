b9430a3e80530c4f7f10f62fc7114be985852991
Shen Yu <yshen@kingbase.com.cn>
2016-10-21 16:01:52 +0800

    initial commit

---
+++ test.c
@@ -0,0 +1,33 @@
+#include "a/b/c.h"
+if(a==b && b!= c)
+{
+	/* some blank line follow */      
+	for(i=1; i<100;i++)
+	{
+		func(a,b, c);
+		func(d,e, f);
+		func(a, b, c);
+	}
+
+	a++;
+	a+b;
+	a+ b;
+	a +b;
+	a+=1;
+
+	a--;
+	a-b;
+	a-=1;
+
+	a=*p;
+	a*b;
+	a/b;
+	a<<1;
+	a>>2;
+	a>=1;
+	a<=2;
+
+	a+b+c+d+e+f;	
+	a==b==c==d;
+	
+}
